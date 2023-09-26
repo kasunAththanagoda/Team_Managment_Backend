2a3c00ea4eb16dd2743145fd80126fe8b09c7a2e
Kasun <kasun@vizumatix.com>
2023-09-26 16:04:25 +0530

    basic structure

---
+++ src/main/java/com/TMA/TeamManagmentApp/TeamManagmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.TMA.TeamManagmentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamManagmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamManagmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TMA/TeamManagmentApp/TeamManagmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TMA.TeamManagmentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
